dd9eaf84e02b14725975acb9fad3b10feccf201a
gabriel <gabri005@gmail.com>
2019-03-01 13:53:20 -0300

    created_at field added to model transaction and some model validation rules added to

--- wallet/models.py
+++ wallet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
+
 from .constants import IN, OUT
 
 
@@ -13,23 +14,50 @@ class Transaction(models.Model):
     TYPES_TRANSACTION = (   
         (OUT, 'OUT'),  
         (IN, 'IN'),
-    ) 
-    ZERO = '0.00'
+    )
     name = models.CharField(max_length=100)
     value = models.FloatField()
     type_transaction = models.SmallIntegerField(choices=TYPES_TRANSACTION)
     wallet = models.ForeignKey(Wallet, related_name='transactions', on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    def _set_value_by_type_transaction(self):
+        if self.value > 0:
+            if self.type_transaction == IN:
+                pass
+            elif self.type_transaction == OUT:
+                self.value = -self.value
+            return float(self.value)
+        else:
+            raise Exception('input value field must be positive !')
+        
+    def _check_type_transaction(self):
+        if self.type_transaction not in [OUT, IN]:
+            raise Exception('invalid value to type_transaction, must be 0 or 1 !')
+    
+    def _check_wallet(self):
+        if not self.wallet:
+            raise Exception('wallet field cannot be null')
 
     @property
-    def value_with_signal(self):
-        signal = '+ ' if self.type_transaction == self.IN else '- '
-        format_value = '{:.2f}'.format(self.value) if self.value > 0 else self.ZERO
-        return signal + format_value
+    def presentation_value(self):
+        value = ' R${:.2f}'
+        if self.type_transaction == IN:
+            value = '+' + value.format(abs(self.value))
+        elif self.type_transaction == OUT:
+            value = '-' + value.format(abs(self.value))
+        return value 
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self._check_wallet()
+            self._check_type_transaction()
+            self._set_value_by_type_transaction()
+        super().save(*args, **kwargs)
     
-    # def check_type_transaction(self):
-        
     def __str__(self):
-        return '{0} - {1}'.format(self.name, self.value_with_signal)
+        value_to_presentation = 'R${:2f}'.format(abs(self.value))
+        return '{0} - {1}'.format(self.name, value_to_presentation)
 
 
 
