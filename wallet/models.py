aa5057f80d5073176fe4369981e3db8c264103ed
gabriel <gabri005@gmail.com>
2019-03-02 01:24:03 -0300

    wallet in transaction model has been changed to NOT NULL

--- wallet/models.py
+++ wallet/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
+from decimal import Decimal
 
-from .constants import IN, OUT
+from .constants import IN_TRANSACTION, OUT_TRANSACTION
 
 
 # Create your models here.
@@ -12,51 +14,48 @@ class Wallet(models.Model):
 
 class Transaction(models.Model):
     TYPES_TRANSACTION = (   
-        (OUT, 'OUT'),  
-        (IN, 'IN'),
+        (OUT_TRANSACTION, 'OUT'),  
+        (IN_TRANSACTION, 'IN'),
     )
     name = models.CharField(max_length=100)
-    value = models.FloatField()
+    value = models.DecimalField(decimal_places=2, max_digits=12)
     type_transaction = models.SmallIntegerField(choices=TYPES_TRANSACTION)
-    wallet = models.ForeignKey(Wallet, related_name='transactions', on_delete=models.CASCADE, null=True)
+    wallet = models.ForeignKey(Wallet, related_name='transactions', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
-
-    def _set_value_by_type_transaction(self):
-        if self.value > 0:
-            if self.type_transaction == IN:
-                pass
-            elif self.type_transaction == OUT:
-                self.value = -self.value
-            return float(self.value)
-        else:
-            raise Exception('input value field must be positive !')
-        
-    def _check_type_transaction(self):
-        if self.type_transaction not in [OUT, IN]:
-            raise Exception('invalid value to type_transaction, must be 0 or 1 !')
-    
-    def _check_wallet(self):
-        if not self.wallet:
-            raise Exception('wallet field cannot be null')
 
     @property
     def presentation_value(self):
         value = ' R${:.2f}'
-        if self.type_transaction == IN:
+        if self.type_transaction == IN_TRANSACTION:
             value = '+' + value.format(abs(self.value))
-        elif self.type_transaction == OUT:
+        elif self.type_transaction == OUT_TRANSACTION:
             value = '-' + value.format(abs(self.value))
         return value 
 
+    def _set_value_by_type_transaction(self):
+        if self.type_transaction == IN_TRANSACTION:
+            pass
+        elif self.type_transaction == OUT_TRANSACTION:
+            self.value = -self.value
+        return Decimal(self.value)  
+        
+    def _check_type_transaction(self):
+        if self.type_transaction not in [OUT_TRANSACTION, IN_TRANSACTION]:
+            raise ValidationError('invalid value to type_transaction, must be 0 or 1 !')
+    
+    def _check_signal_value(self):
+        if self.value < 0:
+            raise ValidationError('set value must be positive')
+
     def save(self, *args, **kwargs):
         if not self.pk:
-            self._check_wallet()
+            self._check_signal_value()
             self._check_type_transaction()
             self._set_value_by_type_transaction()
         super().save(*args, **kwargs)
     
     def __str__(self):
-        value_to_presentation = 'R${:2f}'.format(abs(self.value))
+        value_to_presentation = 'R${:.2f}'.format(abs(self.value))
         return '{0} - {1}'.format(self.name, value_to_presentation)
 
 
